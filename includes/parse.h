a4a6daeb8c1874fc95c99ce0a642045899fbd199
Azedine Ouhadou <xperazazedine@gmail.com>
2022-07-01 12:07:03 +0100

    fix norm1

--- includes/parse.h
+++ includes/parse.h
@@ -6,7 +6,7 @@
 /*   By: aouhadou <aouhadou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 15:46:02 by aouhadou          #+#    #+#             */
-/*   Updated: 2022/07/01 10:10:50 by aouhadou         ###   ########.fr       */
+/*   Updated: 2022/07/01 12:05:22 by aouhadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,16 +65,17 @@ char				**remplir_tab(t_token *node, int start);
 int					count_size(t_token	*node, int start);
 int					remove_quotes(t_command *node);
 
-//epand dollar
+//expand dollar
 void				expand_dollar(t_command *node);
-
-//expander_utils1/2
 int                 check_dollar(t_command	*list);
 char				*dollar_substr1(char *str);
 char				*dollar_substr(char *str);
 int					is_dollar(char *str);
 int					get_env_end(char *str);
 const char			*ft_strstr(const char *str, const char *to_find);
+void                ft_expand(char **dest, char *sub, char *env);
+void                ex_free(char *sub, char *env);
+void                replace_sub(char **str, const char *old, const char *new_);
 
 //files creation
 void				ft_free(char *s1, char *s2);
